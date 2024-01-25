97ef659306be69bff6ffdc6ea3195ef82ba11a5e
jhy <jianghuayu@popmart.com>
2024-01-25 17:05:19 +0800

    feat: list img col

--- src/renderer/pages/Index/components/MyList/index.tsx
+++ src/renderer/pages/Index/components/MyList/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-lonely-if */
 /* eslint-disable import/no-cycle */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable promise/always-return */
 /* eslint-disable promise/catch-or-return */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { List, Image } from 'antd';
 import { PlusCircleOutlined } from '@ant-design/icons';
@@ -40,6 +42,7 @@ export default function Index(props: Iprops) {
 
   const [loading, setLoading] = useState<boolean>(false);
   const [isBottom, setIsBottom] = useState<boolean>(false);
+  const [imgCol, setImgCol] = useState<number>(1);
 
   const addData = async () => {
     changeActiveItem();
@@ -63,6 +66,57 @@ export default function Index(props: Iprops) {
       setIsBottom(true);
     }
   };
+
+  const imgList = [
+    {
+      name: 1,
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+    {
+      name: 2,
+      url: 'atom:///Users/popmart/Documents/fileDaily/file/test.png',
+    },
+    {
+      name: 3,
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+    {
+      name: 4,
+      url: 'atom:///Users/popmart/Documents/fileDaily/file/test.png',
+    },
+    {
+      name: 5,
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+    {
+      name: 6,
+      url: 'atom:///Users/popmart/Documents/fileDaily/file/test.png',
+    },
+    {
+      name: 7,
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ];
+
+  useEffect(() => {
+    if (imgList.length === 1) {
+      setImgCol(1);
+    } else {
+      if (imgList.length < 5) {
+        setImgCol(2);
+      } else {
+        if (imgList.length < 10) {
+          setImgCol(3);
+        } else {
+          if (imgList.length < 17) {
+            setImgCol(4);
+          } else {
+            setImgCol(5);
+          }
+        }
+      }
+    }
+  }, [imgList]);
 
   return (
     <div className={styles.listContainer}>
@@ -130,18 +184,23 @@ export default function Index(props: Iprops) {
                 </div>
 
                 <div className={styles.imgList}>
-                  <div
-                    className={styles.imgItem}
-                    onClick={() => {
-                      window.electron.ipcRenderer.send('open-view-window');
-                    }}
-                  >
-                    <img
-                      width={100}
-                      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                      alt=""
-                    />
-                  </div>
+                  {imgList.map((item: any) => {
+                    return (
+                      <div
+                        key={item.name}
+                        className={[
+                          styles.imgItem,
+                          styles[`imgItem${imgCol}`],
+                        ].join(' ')}
+                        onClick={() => {
+                          window.electron.ipcRenderer.send('open-view-window');
+                        }}
+                        style={{
+                          backgroundImage: `url(${item.url})`,
+                        }}
+                      />
+                    );
+                  })}
                 </div>
                 <div className={styles.bottom}>
                   <span className={styles.time}>
